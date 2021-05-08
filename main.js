f3b44571d3c9fb720ec0e366c3d1dca7848bce00
unknown <rondinonstefi@gmail.com>
2021-05-08 18:14:16 -0300

    seguro-form_funcionando

--- main.js
+++ main.js
@@ -1,4 +1,32 @@
-function mostrarPrecio(precio) {
-    console.log(precio)
+function mostrarPrecio(seguro) {
+    
+    if  (seguro == "basico") {
+        document.querySelector("body > form > div > p:nth-child(2)").style.opacity = "1"
+        document.querySelector("body > form > div > p:nth-child(4)").style.opacity = "0"
+        document.querySelector("body > form > div > p:nth-child(6)").style.opacity = "0"
+    }
+    else if  (seguro == "intermedio") {
+        document.querySelector("body > form > div > p:nth-child(4)").style.opacity = "1"
+        document.querySelector("body > form > div > p:nth-child(2)").style.opacity = "0"
+        document.querySelector("body > form > div > p:nth-child(6)").style.opacity = "0"
 
-}
+
+    }
+    else if  (seguro == "premium") {
+        document.querySelector("body > form > div > p:nth-child(6)").style.opacity = "1"
+        document.querySelector("body > form > div > p:nth-child(2)").style.opacity = "0"
+        document.querySelector("body > form > div > p:nth-child(4)").style.opacity = "0"
+
+
+    }
+    else {
+        console.log("No se pudo elegir un seguro");
+    }
+ }
+
+
+
+// function saludo() {
+//     console.log("saludo")
+//     alert('Hola qué tal')
+// }
